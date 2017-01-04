42bb1db0181a3addf7a9919948d4de2db3fea005
Joshua Gonzales <j@joshua.bio>
2017-01-03 16:30:40 -0800

    phrasings

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,32 @@
 {% include js/isotope.pkgd.min.js %}
 {% include js/dropcap.min.js %}
 
+var phrasings = [];
+$.getJSON("/phrasings.json", function(data) {
+  phrasings = data.phrasings;
+});
+
+function randomNoRepeats(array) {
+  var copy = array.slice(0);
+  return function() {
+    if (copy.length < 1) { copy = array.slice(0); }
+    var index = Math.floor(Math.random() * copy.length);
+    var item = copy[index];
+    copy.splice(index, 1);
+    return item;
+  };
+}
+
+var updatePhrasing = function() {
+  var item = randomNoRepeats(phrasings);
+  console.log(item);
+  $('.phrasing').text(item);
+}
+
+$(document).ready(function() {
+  updatePhrasing();
+});
+
 $(function() {
   $('.post__body p').first().html(function (i, html) {
     return html.replace(/^[^a-zA-Z]*([a-zA-Z])/g, '<span class="dropcap">$1</span>');
@@ -17,4 +43,4 @@ $(function() {
     itemSelector: '.grid-item',
     percentPosition: true
   })
-})
+});
